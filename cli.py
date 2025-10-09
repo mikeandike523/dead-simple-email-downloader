a4cf1d8d29dffe3c9cf4249e71d1ae025d9af3e5
Michael Sohnen <michaelsohnenacademic@gmail.com>
2025-10-09 18:57:04 -0400

    Finished code for sanity checks. Item counts don't match. Need to debug issue with following nextlinks I think.

--- cli.py
+++ cli.py
@@ -127,9 +127,14 @@ def outlook_me():
 
 @outlook.command("folders")
 def outlook_folders():
-    resp = call_route("/outlook/indexing/get-folders", "Fetching folder info...", save_debug_to=".dsed/debug/folders.json")
+    resp = call_route(
+        "/outlook/indexing/get-folders",
+        "Fetching folder info...",
+        save_debug_to=".dsed/debug/folders.json",
+    )
     if resp is None:
         return -1
+
 
 def index_folder_get_top_level_ids(node):
     if not os.path.isfile(f".dsed/index/top-level-messages/{node['id']}.json"):
@@ -177,22 +182,64 @@ def index_folder_get_top_level_ids(node):
             message_ids.extend(resp.data["messageIds"])
             print(f"Discovered {len(message_ids)} so far...")
             next_link = resp.data.get("nextLink", None)
-        with open(f".dsed/index/top-level-messages/{node['id']}.json", "w", encoding="utf-8") as f:
+        with open(
+            f".dsed/index/top-level-messages/{node['id']}.json", "w", encoding="utf-8"
+        ) as f:
             f.write(json.dumps(message_ids))
-            print(colored(f"Indexed messages in folder {node['name']} ({node['id']}).", "green"))
-        with open(f".dsed/index/top-level-messages/{node['id']}.json", "r", encoding="utf-8") as f:
+            print(
+                colored(
+                    f"Indexed messages in folder {node['name']} ({node['id']}).",
+                    "green",
+                )
+            )
+        with open(
+            f".dsed/index/top-level-messages/{node['id']}.json", "r", encoding="utf-8"
+        ) as f:
             message_ids = json.load(f)
 
-
     return True
+
 
 def index_folder_sanity_check(node):
 
     top_level_messages_path = f".dsed/index/top-level-messages/{node['id']}.json"
     if not os.path.isfile(top_level_messages_path):
-        print(colored(f"No top-level messages found in folder {node['name']} ({node['id']}). Fatal error.", "red"))
+        print(
+            colored(
+                f"No top-level messages found in folder {node['name']} ({node['id']}). Fatal error.",
+                "red",
+            )
+        )
         print(colored("Check that previous steps ran correctly.", "red"))
         return False
+    folder_metadata = call_route(
+        "/outlook/indexing/get-folder-metadata",
+        "Fetching folder metadata...",
+        method="POST",
+        json_body={"folderId": node["id"]},
+    )
+    if folder_metadata is None:
+        return False
+    if not isinstance(folder_metadata.data, dict) or not isinstance(
+        folder_metadata.data["counts"], dict
+    ) or not isinstance(folder_metadata.data["counts"]["totalItemCount"], int):
+        print(
+            colored(
+                "Got successful HTTP status but invalid response data.",
+                "red",
+            )
+        )
+    totalItemCount = folder_metadata.data["counts"]["totalItemCount"]
+    with open(top_level_messages_path, "r", encoding="utf-8") as f:
+        message_ids = json.load(f)
+    indexedItemCount = len(message_ids)
+    print(f"Indexed: {indexedItemCount}\tTotal: {totalItemCount}")
+    if indexedItemCount < totalItemCount:
+        print(f"{totalItemCount - indexedItemCount} messages may have arrived or been moved in since indexing started.")
+    if indexedItemCount > totalItemCount:
+        print(f"{indexedItemCount - totalItemCount} messages may have been deleted or moved since indexing started.")
+
+    return True
 
 @outlook.command("index")
 @click.option("--reset", is_flag=True, default=False, help="Reset the index.")
@@ -205,20 +252,25 @@ def outlook_index(reset=False):
         return 0
     try:
 
-    
         if not os.path.isfile(".dsed/index/folders.json"):
-            resp_folders = call_route("/outlook/indexing/get-folders", "Fetching folder info...")
+            resp_folders = call_route(
+                "/outlook/indexing/get-folders", "Fetching folder info..."
+            )
             if resp_folders is None:
                 return -1
             folder_data = resp_folders.data
 
             with open(".dsed/index/folders.json", "w", encoding="utf-8") as f:
                 f.write(json.dumps(resp_folders.data, indent=2))
-                print(colored("Folder information saved to.dsed/index/folders.json", "green"))
+                print(
+                    colored(
+                        "Folder information saved to.dsed/index/folders.json", "green"
+                    )
+                )
 
         with open(".dsed/index/folders.json", "r", encoding="utf-8") as f:
             folder_data = json.load(f)
-    
+
         os.makedirs(".dsed/index/top-level-messages", exist_ok=True)
 
         folders = []
@@ -236,30 +288,31 @@ def outlook_index(reset=False):
             print("\t" + folder_name)
 
         for i, (folder_name, node) in enumerate(folders):
-            print(f"Getting Top Level IDs for Folder {i+1}/{len(folders)}: {folder_name}")
+            print(
+                f"Getting Top Level IDs for Folder {i+1}/{len(folders)}: {folder_name}"
+            )
 
             if not index_folder_get_top_level_ids(node):
                 print(colored(f"Failed to get top level IDs for {folder_name}", "red"))
                 return -1
-            
+
         print(colored("All folders top-level-indexed successfully.", "green"))
-        
+
         print("Performing sanity checks...")
 
-        
         for i, (folder_name, node) in enumerate(folders):
             print(f"Sanity Check for Folder {i+1}/{len(folders)}: {folder_name}")
 
             if not index_folder_sanity_check(node):
                 print(colored(f"Sanity check failed for {folder_name}", "red"))
                 return -1
-            
+
         return 0
 
     except KeyboardInterrupt:
         print("Process aborted by user.")
         return -1
-    
+
 
 if __name__ == "__main__":
     cli()
